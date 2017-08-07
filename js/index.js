3f1a1afe1951316cd92ab26660e31b8f5b33ab62
Kostya <kostyadepa@ukr.net>
2017-08-07 08:24:08 +0000

    add menu

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const MENU_TOP_BORDER = 500;
+const $menu = document.querySelector('nav');
+
+window.addEventListener('scroll', function() {
+	if (window.document.body.scrollTop > MENU_TOP_BORDER) {
+    	$menu.classList.remove('bottom');
+		$menu.classList.add('top');
+	} else {
+    	$menu.classList.remove('top');
+		$menu.classList.add('bottom');
+    }
+});
